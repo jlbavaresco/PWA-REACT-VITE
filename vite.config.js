6f6dee5427575dacd1fb43b742df0e381f85cbb2
jlbavaresco@gmail.com <jlbavaresco@gmail.com>
2022-12-14 14:55:39 -0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,39 @@
+// vite.config.ts
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { VitePWA } from "vite-plugin-pwa"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "vite.svg"],
+      manifest: {
+        name: "Vite PWA",
+        short_name: "VitePWA",
+        description: "Reat PWA example using vite-plugin-pwa.",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: false,
+      },
+    }),
+  ],
+})
